8eac6a3fb49016580bad5fc7ede94d02592fab67
kommune <kommune.000@gmail.com>
2018-03-01 15:02:49 +0800

    Humanized enum categories

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -3,5 +3,5 @@ class Agency < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
 
-  enum category: [:financial, :family, :food_rations, :donations, :health, :elderly, :children, :teens, :unemployment, :homeless, :mental_health, :addictions, :special_needs, :disability]
+  enum category: [:financial, :family, :food_rations, :donations, :health, :elderly, :children, :teens, :employment, :homeless, :mental_health, :addictions, :counselling, :legal, :special_needs, :disability, :miscellaneous]
 end
