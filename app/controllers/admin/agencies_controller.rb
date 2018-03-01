59a0230d1ad8da2d8a346a8b325530a849a9eb82
zainudinsainal <supernova37@gmail.com>
2018-03-01 21:27:31 +0800

    Delete role in user

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -1,6 +1,5 @@
 class Admin::AgenciesController < ApplicationController
 
-  before_action :authenticate_admin
   before_action :authenticate_user!
   before_action :set_agency, only:  [:show, :edit, :update, :destroy]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def admin?
-    self.role == "admin"
-  end
   
 end
