2027b16af00d279cbc325793cfb0fc957485abbe
zainudinsainal <supernova37@gmail.com>
2018-03-01 21:30:22 +0800

    Generate devise admin model

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -1,6 +1,6 @@
 class Admin::AgenciesController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_admin!
   before_action :set_agency, only:  [:show, :edit, :update, :destroy]
 
   def index
