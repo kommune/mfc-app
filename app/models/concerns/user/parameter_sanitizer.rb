41bf880f085df6a4f87d657198f912171d67ba9c
zainudinsainal <supernova37@gmail.com>
2018-03-04 02:23:49 +0800

    Add sign in for user params sanitizer, edit user agencies index view

--- app/models/concerns/user/parameter_sanitizer.rb
+++ app/models/concerns/user/parameter_sanitizer.rb
@@ -1,6 +1,7 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
+    permit(:sign_in, keys: [:username, :password, :remember_me])
     permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me, :name, :postal_code, :birth_date, :gender, :marital_status, :children ])
     permit(:account_update, keys: [:username, :password, :password_confirmation ])
   end
