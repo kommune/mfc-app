a54aee0b2f9620c5724d6f3f97c7cad37aa02952
zainudinsainal <supernova37@gmail.com>
2018-03-22 07:19:29 +0800

    Edit admin page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :agencies
     resources :agencyusers
-    resources :categories, except: [:show, :new]
+    resources :categories
     resources :users, only: [:index, :show, :edit, :update, :destroy]
     root to: "agencies#index"
   end
