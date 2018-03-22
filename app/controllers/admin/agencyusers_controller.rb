449a425b00f131d518a802503c58d899bb4d6f4d
zainudinsainal <supernova37@gmail.com>
2018-03-23 07:56:03 +0800

    Generate admin/controller for agencyuser and setup rubocop

---
+++ app/controllers/admin/agencyusers_controller.rb
@@ -0,0 +1,43 @@
+class Admin::AgencyusersController < ApplicationController
+
+  layout 'admin'
+
+  before_action :set_user, only:  [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated"
+      redirect_to admin_user_path(@user)
+    else
+      flash.now[:alert] = "User was failed to update"
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:alert] = "User was deleted"
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :postal_code, :birth_date, :gender, :marital_status, :children)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+end
