de99561219d18c4cc12f1235c40eba9baf21dd81
zainudinsainal <supernova37@gmail.com>
2018-03-23 11:22:50 +0800

    Edit routes file

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,6 @@ Rails.application.routes.draw do
 
   resources :message_boards, only: [:new, :create, :show, :index]
 
-  namespace :agency do
-    resources :message_boards, only: [:new, :create, :show, :index]
-  end
-
-  root to: "agencies#index"
-
   root to: "agencies#home"
 
   namespace :agency do
