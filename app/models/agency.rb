1d4b7be4391fe76fef1604ef4d2473bc1d85e352
zainudinsainal <supernova37@gmail.com>
2018-03-24 13:59:17 +0800

    Edit seeds data for areas

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -4,7 +4,7 @@ class Agency < ApplicationRecord
 
   validates :name, presence: true
 
-  enum area: [ :north, :south, :east, :west, :central ]
+  enum area: [ :north, :south, :east, :west, :central, :all_areas ]
 
   has_many :agencies_categories, dependent: :destroy
   has_many :categories, through: :agencies_categories
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require 'csv'
 CSV.foreach(Rails.root.join("db/seeds_data/agencies.csv"), headers: true) do |row|
-  Agency.find_or_create_by!(name: row[0], office_number: row[1], fax_number: row[2], street_name: row[3], postal_code: row[4], email: row[5], website: row[6], opening_hours: row[7], criteria: row[8], description: row[9])
+  Agency.find_or_create_by!(name: row[0], office_number: row[1], fax_number: row[2], street_name: row[3], postal_code: row[4], email: row[5], website: row[6], opening_hours: row[7], criteria: row[8], description: row[9], area: row[13])
 end
 
 puts "Agency created"
@@ -18,11 +18,11 @@ end
 puts "Agencyuser created"
 
 Admin.create!(name: "Admin", email: "admin@example.com", password: "123123")
-puts "Default admin created!"
+puts "Default admin created"
 
 User.create!(name: "User1", username: "user1", password: "123123", postal_code: "650175", email: "user1@montfortcare.org.sg", birth_date: Date.new(1958, 6, 22))
 User.create!(name: "User2", username: "user2", password: "123123", postal_code: "650175", email: "user2@montfortcare.org.sg", birth_date: Date.new(1978, 6, 22))
-puts "2 users created!"
+puts "2 users created"
 
 (1..49).each do |x|
   Agency.find(x).category_ids = [1,2,6,7,8,10,11,12,13,17]
