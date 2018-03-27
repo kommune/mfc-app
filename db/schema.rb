5d2fd8e1ef936731ad79774500c774ce4a3ef648
Liyan <liyan@liyanchua.com>
2018-03-27 10:33:52 +0800

    Update user and message board model, create subscription model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323234816) do
+ActiveRecord::Schema.define(version: 20180327022034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,14 @@ ActiveRecord::Schema.define(version: 20180323234816) do
     t.datetime "updated_at", null: false
     t.index ["message_board_id"], name: "index_messages_on_message_board_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "message_board_id"
+    t.integer "user_id"
+    t.integer "agencyuser_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
