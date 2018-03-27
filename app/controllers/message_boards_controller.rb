887e20c556b4908e56decbbbd1a8c8088df20093
Liyan <liyan@liyanchua.com>
2018-03-27 13:23:55 +0800

    Update index view for message board

--- app/controllers/message_boards_controller.rb
+++ app/controllers/message_boards_controller.rb
@@ -3,8 +3,12 @@ require 'securerandom'
 class MessageBoardsController < ApplicationController
 
   def index
-    message_boards = current_user.message_boards
-    @existing_message_boards_users = current_user.existing_message_boards_users
+    if current_user
+      message_boards = current_user.message_boards
+      @existing_message_boards_users = current_user.existing_message_boards_users
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def new
@@ -12,18 +16,18 @@ class MessageBoardsController < ApplicationController
   end
 
   def create
-    @other_user = Agencyuser.find(params[:other_user])
-    @message_board = find_chat(@other_user) || MessageBoard.new(identifier: SecureRandom.hex)
+    @agency_user = Agencyuser.find(params[:id])
+    @message_board = find_chat(@agency_user) || MessageBoard.new(identifier: SecureRandom.hex)
     if !message_board.persisted?
       @message_board.save
       @message_board.subscriptions.create(user_id: current_user.id)
-      @message_board.subscriptions.create(agencyuser_id: @other_user.id)
+      @message_board.subscriptions.create(agencyuser_id: @agency_user.id)
     end
-    redirect_to message_board_path(current_user, @message_board, :other_user => @other_user.id)
+    redirect_to message_board_path(current_user, @message_board)
   end
 
   def show
-    @other_user = User.find(params[:other_user])
+    @agency_user = Agencyuser.find(params[:id])
     @message_board = MessageBoard.find_by(id: params[:id])
     @message = Message.new
   end
@@ -43,7 +47,7 @@ class MessageBoardsController < ApplicationController
   end
 
   def message_board_params
-    params.require(:message_board).permit(:title)
+    params.require(:message_board).permit(:identifier)
   end
 
 end
