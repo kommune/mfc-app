f008e60472a54808546290d934d0ad7e2668c92a
Liyan <liyan@liyanchua.com>
2018-03-28 11:47:41 +0800

    Fix message board user chats

--- app/controllers/message_boards_controller.rb
+++ app/controllers/message_boards_controller.rb
@@ -3,42 +3,34 @@ require 'securerandom'
 class MessageBoardsController < ApplicationController
 
   def index
-    if current_user
       message_boards = current_user.message_boards
       @existing_message_boards_users = current_user.existing_message_boards_users
-    else
-      redirect_to new_user_session_path
-    end
-  end
-
-  def new
-    @message_board = MessageBoard.new
   end
 
   def create
-    @agency_user = Agencyuser.find(params[:id])
-    @message_board = find_chat(@agency_user) || MessageBoard.new(identifier: SecureRandom.hex)
-    if !message_board.persisted?
+    @other_user = User.find(params[:other_user])
+    @message_board = find_chat(@other_user) || MessageBoard.new(identifier: SecureRandom.hex)
+    if !@message_board.persisted?
       @message_board.save
       @message_board.subscriptions.create(user_id: current_user.id)
-      @message_board.subscriptions.create(agencyuser_id: @agency_user.id)
+      @message_board.subscriptions.create(user_id: @other_user.id)
     end
-    redirect_to message_board_path(current_user, @message_board)
+    redirect_to user_message_board_path(current_user, @message_board, :other_user => @other_user.id)
   end
 
   def show
-    @agency_user = Agencyuser.find(params[:id])
+    @other_user = User.find(params[:other_user])
     @message_board = MessageBoard.find_by(id: params[:id])
     @message = Message.new
   end
 
   private
 
-  def find_chat(agency_user)
+  def find_chat(second_user)
     message_boards = current_user.message_boards
     message_boards.each do |message_board|
       message_board.subscriptions.each do |s|
-        if s.agencyuser_id == agency_user.id
+        if s.user_id == second_user.id
           return message_board
         end
       end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,4 @@
 class Subscription < ApplicationRecord
   belongs_to :message_board
   belongs_to :user
-  belongs_to :agencyuser
 end
