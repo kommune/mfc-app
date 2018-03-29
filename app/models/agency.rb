e7babada0a14c5917fcbb679f486f76b60d690d0
kommune <kommune.000@gmail.com>
2018-03-29 10:47:59 +0800

    Do area nav by search

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,6 +1,19 @@
 class Agency < ApplicationRecord
 
   searchkick word_start: [:name, :street_name, :description]
+
+  def search_data
+    {
+      name: name,
+      street_name: street_name,
+      postal_code: postal_code,
+      office_number: office_number,
+      criteria: criteria, 
+      description: description, 
+      category_ids: category_ids, 
+      area: area
+    }
+  end
   
   validates :name, presence: true
 
@@ -9,5 +22,6 @@ class Agency < ApplicationRecord
   has_many :agencies_categories, dependent: :destroy
   has_many :categories, through: :agencies_categories
   has_one :agencyuser
+
 
 end
