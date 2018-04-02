bcbf5c976e322d2c4254eea4028b8e054ead1b2d
Liyan <liyan@liyanchua.com>
2018-04-02 14:11:27 +0800

    Message board changes

--- db/schema.rb
+++ db/schema.rb
@@ -227,7 +227,7 @@ ActiveRecord::Schema.define(version: 20180402033126) do
     t.string "name", null: false
     t.string "username", null: false
     t.integer "postal_code"
-    t.date "birth_date", default: "2018-04-01", null: false
+    t.date "birth_date", default: "2018-04-02", null: false
     t.integer "gender", default: 0
     t.integer "marital_status", default: 0
     t.integer "children", default: 0
