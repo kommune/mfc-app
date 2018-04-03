1dfb23778d22ff2b3c6432442190be5971771aa3
kommune <kommune.000@gmail.com>
2018-04-03 15:23:17 +0800

    Change backticks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => ‘mfc-dir-app.herokuapp.com’,
-    :address => ‘smtp.sendgrid.net’,
+    :domain => 'mfc-dir-app.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
